7deed08668eb169bc5cd86894536d6c11a2a358d
MasterMax13124 <mastermax13124@gmail.com>
2021-01-17 09:36:33 +0100

    Removed useless try-except, added if clause to avoid errors

--- pdfcrawler.py
+++ pdfcrawler.py
@@ -1,23 +1,25 @@
 import pdfplumber
 import os
 
-os.chdir("tgnotes")
+#os.chdir("tgnotes")
 
-megafile = open("megafile.txt", "w+")
 
 pdflist = os.listdir()
+megafile = open("megafile.txt", "w+")
 index = 0
+
 for filename in pdflist:
-    print(index)
-    pdf = pdfplumber.open(pdflist[index])
-    try:
+    if filename[-3:] == "pdf":
+
+        print(index)
+        pdf = pdfplumber.open(pdflist[index])
         text = pdf.pages[0].extract_text()
-    except:
         print("Error occured in file " + index + " " + filename)
 
-    megafile.write("\n\n Text from " + filename + "\n" + text)
+        megafile.write("\n\n Text from " + filename + "\n" + text)
 
-    pdf.close()
+        pdf.close()
     index = index +1
+
 
 megafile.close()
