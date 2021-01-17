b3b83102c77457f6e99ad000d5cdaa944b8d2677
MasterMax13124 <mastermax13124@gmail.com>
2021-01-17 09:56:46 +0100

    Removed misleading error message

--- pdfcrawler.py
+++ pdfcrawler.py
@@ -10,14 +10,10 @@ index = 0
 
 for filename in pdflist:
     if filename[-3:] == "pdf":
-
         print(index)
         pdf = pdfplumber.open(pdflist[index])
         text = pdf.pages[0].extract_text()
-        print("Error occured in file " + index + " " + filename)
-
         megafile.write("\n\n Text from " + filename + "\n" + text)
-
         pdf.close()
     index = index +1
 
