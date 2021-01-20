f265df037c0dda15e0b8d8377430b54ddbd48349
MasterMax13124 <mastermax13124@gmail.com>
2021-01-20 17:52:43 +0100

    Added ability to pick name for megafile.txt

--- pdfcrawler.py
+++ pdfcrawler.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 pathname = input("Please input the path to your pdf folder: ")
+
 if pathname.find("/") == -1:
     pathname = os.getcwd() + "/" + pathname
 os.chdir(pathname)
@@ -10,19 +11,20 @@ os.chdir(pathname)
 pdflist = os.listdir()
 print("Found " + str(len(pdflist)) + " pdf-files")
 
-if os.path.isfile(pathname + "/megafile.txt"):
-    yesno = input("Do you want to overwrite the existing megafile.txt? (Y/n)")
+megafilename = input("PLease input a name for the resulting txt file (without .txt): ") + ".txt"
+
+if os.path.isfile(pathname + "/" + megafilename):
+    yesno = input("Do you want to overwrite the existing " + megafilename + " (Y/n)")
     if yesno.lower() == "no" or yesno.lower() == "n":
         print("Exiting...")
         sys.exit()
 
 
-megafile = open("megafile.txt", "w+")
+megafile = open(megafilename, "w+")
 index = 0
 
 for filename in pdflist:
     if filename[-3:] == "pdf":
-        #print(index)
         print("Reading file " + filename)
         pdf = pdfplumber.open(filename)
         text = pdf.pages[0].extract_text()
